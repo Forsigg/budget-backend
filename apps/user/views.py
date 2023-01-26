aaac4154a73a41df6098a3f183b979add7190033
forsigg <al.berhmiller@gmail.com>
2023-01-26 22:18:30 +0700

    add user_delete_view

--- apps/user/views.py
+++ apps/user/views.py
@@ -9,13 +9,13 @@ from . import crud
 users_router = APIRouter(prefix='/users', tags=['users'])
 
 
-@users_router.get('/users/', response_model=list[schemas.UserBase], tags=['users'])
+@users_router.get('/', response_model=list[schemas.UserBase], tags=['users'])
 def get_users_view(db: Session = Depends(get_db)):
     users = crud.get_all_users(db)
     return users
 
 
-@users_router.post('/users/', response_model=schemas.UserBase, tags=['users'])
+@users_router.post('/', response_model=schemas.UserBase, tags=['users'])
 def create_user_view(user: schemas.UserCreate, db: Session = Depends(get_db)):
     db_user = crud.get_user_by_email(db, user.email)
     if db_user:
@@ -23,3 +23,8 @@ def create_user_view(user: schemas.UserCreate, db: Session = Depends(get_db)):
     return crud.create_user(db, user)
 
 
+@users_router.delete('/{user_id}', response_model=schemas.UserDeleteResponse, tags=['users'])
+def delete_user_view(user_id: int, db: Session = Depends(get_db)):
+    is_deleted = crud.delete_user_by_id(db, user_id)
+    detail = {} if is_deleted else {'error': f'User with id {user_id} not find'}
+    return schemas.UserDeleteResponse(id=user_id, deleted=is_deleted, detail=detail)
